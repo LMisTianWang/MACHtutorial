dc072a600d280488de1ac12edb4d74c2005d9fae
Nick Bons <nbons@umich.edu>
2018-02-26 13:21:49 -0500

    Updated comments for structural analysis script.

--- tutorial/struct/analysis/struct_run.py
+++ tutorial/struct/analysis/struct_run.py
@@ -2,12 +2,9 @@
 #       Import modules
 # ==============================================================================
 import numpy
-import argparse
-from mpi4py import MPI
 from baseclasses import *
 from tacs import *
 from repostate import *
-from pyoptsparse import *
 
 # ==============================================================================
 #       Set up structural problem
@@ -29,22 +26,23 @@ FEASolver = pytacs.pyTACS(bdfFile, options=structOptions)
 #       Set up design variable groups
 # ==============================================================================
 # Give each rib its own design variable group under the 'RIBS' category
-# for i in xrange(1,19):
-#     FEASolver.addDVGroup('RIBS', include='RIB.%2.2d'%i)
-FEASolver.addDVGroup('RIBS', include='RIB')
+for i in xrange(1,19):
+    FEASolver.addDVGroup('RIBS', include='RIB.%2.2d'%i)
 
 # Split each spar into 9 design variable groups
 FEASolver.addDVGroup('SPARS', include='SPAR.00', nGroup=9)
 FEASolver.addDVGroup('SPARS', include='SPAR.09', nGroup=9)
 
-# Stringers
+# Split stringers into three chordwise groups: front 3, middle 2, and back 3
 groups = [['L_STRING.01', 'L_STRING.02', 'L_STRING.03'],
           ['L_STRING.04', 'L_STRING.05'],
           ['L_STRING.06', 'L_STRING.07', 'L_STRING.08']]
 
+# Now split stringers into 9 spanwise groups (total 27 stringer groups)
 for group in groups:
     FEASolver.addDVGroup('STRINGERS', include=group, nGroup=9)
 
+# Same thing for the upper skin stringers
 groups = [['U_STRING.01', 'U_STRING.02', 'U_STRING.03'],
           ['U_STRING.04', 'U_STRING.05'],
           ['U_STRING.06', 'U_STRING.07', 'U_STRING.08']]
@@ -52,7 +50,7 @@ groups = [['U_STRING.01', 'U_STRING.02', 'U_STRING.03'],
 for group in groups:
     FEASolver.addDVGroup('STRINGERS', include=group, nGroup=9)
 
-# Skins
+# Group up the skins after the side-of-body (2 skin panels per group)
 boundLists = [
     ['SPAR.00','SPAR.09','RIB.02','RIB.04'],
     ['SPAR.00','SPAR.09','RIB.04','RIB.06'],
@@ -77,25 +75,33 @@ for i in xrange(1,19):
 FEASolver.addDVGroup('U_SKIN', include=u_skins)
 FEASolver.addDVGroup('L_SKIN', include=l_skins)
 
+# ==============================================================================
+#       Set-up constitutive properties for each DVGroup
+# ==============================================================================
 def conCallBack(dvNum, compDescripts, userDescript, specialDVs, **kargs):
-    rho_2024 = 2780
-    E_2024 = 73.1e9
-    ys_2024 = 324e6
+    rho_2024 = 2780 # kg/m^3
+    E_2024 = 73.1e9 # Pa
+    ys_2024 = 324e6 # Pa
     nu = 0.33
-    t = .02
-    tMin = 0.0016 # 1/16"
-    tMax = 0.020
+    t = .02 # m
+    tMin = 0.0016 # m
+    tMax = 0.020 # m
     kcorr = 5.0/6.0
     con = constitutive.isoFSDTStiffness(rho_2024, E_2024, nu, kcorr,
                                         ys_2024, t, dvNum, tMin, tMax)
     scale = [100.0]
     return con, scale
 
+# TACS will now create the constitutive objects for the different DV groups
 FEASolver.createTACSAssembler(conCallBack)
 
+# Write out components to visualize design variables (contour of dv0)
 FEASolver.writeDVVisualization('dv_visual.f5')
-# --------------- Add Functions -------------------------
-# Mass Functions
+
+# ==============================================================================
+#       Add functions
+# ==============================================================================
+# Mass functions
 FEASolver.addFunction('mass', functions.StructuralMass)
 FEASolver.addFunction('uSkin', functions.StructuralMass, include='U_SKIN')
 FEASolver.addFunction('lSkin', functions.StructuralMass, include='L_SKIN')
@@ -103,23 +109,45 @@ FEASolver.addFunction('leSpar', functions.StructuralMass, include=['SPAR.00'])
 FEASolver.addFunction('teSpar', functions.StructuralMass, include=['SPAR.09'])
 FEASolver.addFunction('ribs', functions.StructuralMass, include=['RIBS'])
 
-# KS Functions
-loadFactor = 2.5
+# KS aggregated von Mises stress functions
+safetyFactor = 1.5
 KSWeight = 80.0
 ks0 = FEASolver.addFunction('ks0', functions.AverageKSFailure, KSWeight=KSWeight,
-                            include=['RIBS','SPARS'], loadFactor=loadFactor)
+                            include=['RIBS','SPARS'], loadFactor=safetyFactor)
 ks1 = FEASolver.addFunction('ks1', functions.AverageKSFailure,  KSWeight=KSWeight,
-                            include=['U_SKIN','U_STRING'], loadFactor=loadFactor)
+                            include=['U_SKIN','U_STRING'], loadFactor=safetyFactor)
 ks2 = FEASolver.addFunction('ks2', functions.AverageKSFailure, KSWeight=KSWeight,
-                            include=['L_SKING','L_STRING'], loadFactor=loadFactor)
+                            include=['L_SKING','L_STRING'], loadFactor=safetyFactor)
 
-FEASolver.addFunction('max0',functions.MaxFailure, include=ks0, loadFactor=loadFactor)
-FEASolver.addFunction('max1',functions.MaxFailure, include=ks1, loadFactor=loadFactor)
-FEASolver.addFunction('max2',functions.MaxFailure, include=ks2, loadFactor=loadFactor)
+# Max failure functions
+FEASolver.addFunction('max0', functions.MaxFailure, include=ks0, loadFactor=safetyFactor)
+FEASolver.addFunction('max1', functions.MaxFailure, include=ks1, loadFactor=safetyFactor)
+FEASolver.addFunction('max2', functions.MaxFailure, include=ks2, loadFactor=safetyFactor)
 
+# ==============================================================================
+#       Add loads
+# ==============================================================================
+# Add point load to wing tip
+F = numpy.array([0.0, 3E5, 0.0]) #N
+pt = numpy.array([8.0208, -0.0885, 14.000])
+FEASolver.addPointLoads(sp, pt, F)
+
+# Add distributed load to tip rib
+FEASolver.addLoadToComponents(sp, FEASolver.selectCompIDs(['RIB.18']), F=F)
+
+# Add pressure load (100 kPa) to upper skin of the wing
+FEASolver.addPressureLoad(sp, 100E3, include='U_SKIN')
+
+# Add inertial (gravity) loads
+FEASolver.addInertialLoad(sp)
+
+# ==============================================================================
+#       Evaluate functions
+# ==============================================================================
 FEASolver(sp)
 funcs = {}
 FEASolver.evalFunctions(sp, funcs)
 print funcs
+
 # Write the final solution
 FEASolver.writeOutputFile(sp.name + '_final.f5')
